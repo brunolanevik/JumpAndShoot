7b324ca0ae985893bf5154cc76f4a8383ef60e91
Bruno <bruno.lanevik@gmail.com>
2012-05-24 21:32:27 +0200

    Added main class

--- Jumpandshoot/src/com/gmail/bruno/jumpandshoot/Main.java
+++ Jumpandshoot/src/com/gmail/bruno/jumpandshoot/Main.java
@@ -1,9 +1,15 @@
 package com.gmail.bruno.jumpandshoot;
 
-import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
 
-public class Main extends Application{
+public class Main extends SimpleApplication{
 	public static void main(String[] args){
+		
+	}
+
+	@Override
+	public void simpleInitApp() {
+		// TODO Auto-generated method stub
 		
 	}
 
