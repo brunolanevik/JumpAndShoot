18971dff47cdeee1632481ce646b54d6570c81a9
Bruno <bruno.lanevik@gmail.com>
2012-05-31 15:37:29 +0300

    fixed powerups

--- Jumpandshoot/src/com/gmail/bruno/jumpandshoot/Level/Level.java
+++ Jumpandshoot/src/com/gmail/bruno/jumpandshoot/Level/Level.java
@@ -13,11 +13,27 @@ public abstract class Level {
 	LevelObject[] level;
 	protected float size;
 	protected String name;
+	protected PowerUp[] powerUps;
 	
 	public void setLevelObject(LevelObject[] level){
 		this.level = level;
 	} 
 	public LevelObject[] getLevelObject(){
 		return level;
+	}
+	public void setSize(float size){
+		this.size = size;
+	}
+	public float getSize(){
+		return size;
+	}
+	public void setPowerUps(PowerUps[] powerUps){
+		this.powerUps = powerUps;
+	}
+	public PowerUps[] getPowerUps{
+		return powerUps;
+	}
+	public void numPowerUps(){
+		return powerIps.length;
 	}
 }
