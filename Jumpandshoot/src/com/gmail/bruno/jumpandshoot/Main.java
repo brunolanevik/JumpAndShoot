13a1c0a2b19fe026f75ea7965b0906d8046bd752
Bruno <bruno.lanevik@gmail.com>
2012-07-03 12:05:43 +0300

    Added basic logic for init

--- Jumpandshoot/src/com/gmail/bruno/jumpandshoot/Main.java
+++ Jumpandshoot/src/com/gmail/bruno/jumpandshoot/Main.java
@@ -9,10 +9,13 @@ import com.jme3.app.SimpleApplication;
  *
  * 2012-05-27
  * 
- * Copyright © 2012, Bruno Gustav Lanevik. All rights reserved.
+ * Copyright ï¿½ 2012, Bruno Gustav Lanevik. All rights reserved.
  */
 
 public class Main extends SimpleApplication{
+	private List<Entity entities> entity = new List<Entity>();
+	private Render render;
+	private Player player;
 	public static void main(String[] args){
 		Main app = new Main();
 		app.start();
@@ -21,7 +24,9 @@ public class Main extends SimpleApplication{
 	@Override
 	public void simpleInitApp() {
 		// TODO Auto-generated method stub
-		
+		Entity.setActiveComponents();
+		entity = Entity.getActiveComponents()
+		render = new Render(entity);
 	}
 
 }
